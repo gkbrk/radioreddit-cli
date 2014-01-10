eb6bf23b622cc7cccac612b31ef84a7cc678203b
gkbrk <webdosusb@gmail.com>
2014-01-10 20:33:35 +0200

    Added the base script.

---
+++ radioreddit-cli.py
@@ -0,0 +1,23 @@
+import subprocess, sys, re
+
+if sys.argv.__len__() > 1:
+    stream_name = sys.argv[1]
+else:
+    stream_name = "main"
+
+stream_name_to_url = {
+    "indie": "http://173.231.136.91:8070/"
+}
+
+player = subprocess.Popen(["mplayer", stream_name_to_url[stream_name]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+try:
+    while not player.poll():
+        player_line = player.stdout.readline()
+        if player_line.startswith("ICY Info: "):
+            song_name = re.match("ICY Info: StreamTitle='(.*?)';StreamUrl='';", player_line).group(1)
+            print "New song! %s" % song_name
+except KeyboardInterrupt:
+    player.kill()
+except Exception, e:
+    print e
