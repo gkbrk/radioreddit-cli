c2d56b28833c0cdfab2b52713f2dd32c0e6bf562
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-07-12 14:47:15 +0300

    Some refactoring. Python3 support.

--- radioreddit-cli.py
+++ radioreddit-cli.py
@@ -1,11 +1,8 @@
-import subprocess, sys, re
+import subprocess
+import sys
+import re
 
-if sys.argv.__len__() > 1:
-    stream_name = sys.argv[1]
-else:
-    stream_name = "main"
-
-stream_name_to_url = {
+streams = {
     "main": "http://173.231.136.91:8000/",
     "random": "http://173.231.136.91:8050/",
     "rock": "http://173.231.136.91:8020/",
@@ -13,25 +10,33 @@ stream_name_to_url = {
     "indie": "http://173.231.136.91:8070/"
 }
 
-try:
-    player = subprocess.Popen(["mplayer", stream_name_to_url[stream_name]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-except KeyError:
-    print "Usage: %s [station]" % sys.argv[0]
-    print "Available stations: %s" % ", ".join(sorted(stream_name_to_url))
-    print "Default stream: main"
-    sys.exit(1)
-except OSError as err:
-    if err.errno == 2:
-        print "You need to install mplayer to play the streams. (apt-get install mplayer)"
+def main():
+    if len(sys.argv) > 1:
+        stream_name = sys.argv[1]
+    else:
+        stream_name = "main"
+
+    try:
+        player = subprocess.Popen(["mplayer", streams[stream_name]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    except KeyError:
+        print("Usage: {} [station]".format(sys.argv[0]))
+        print("Available stations: {}".format(", ".join(streams)))
+        print("Default stream: main")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("You need to have mplayer to use radioreddit.")
         sys.exit(1)
 
-try:
-    while not player.poll():
-        player_line = player.stdout.readline()
-        if player_line.startswith("ICY Info: "):
-            song_name = re.match("ICY Info: StreamTitle='(.*?)';StreamUrl='';", player_line).group(1)
-            print "New song! %s" % song_name
-except KeyboardInterrupt:
-    player.kill()
-except Exception, e:
-    print e
+    try:
+        while not player.poll():
+            player_line = player.stdout.readline().decode("utf-8")
+            if player_line.startswith("ICY Info: "):
+                song_name = re.match("ICY Info: StreamTitle='(.*?)';StreamUrl='';", player_line).group(1)
+                print("New song! {}".format(song_name))
+    except KeyboardInterrupt:
+        player.kill()
+    except Exception as e:
+        print("Exception: {}".format(e))
+
+if __name__ == "__main__":
+    main()
